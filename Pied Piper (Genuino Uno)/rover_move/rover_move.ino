30e1014824a5c230a1d73e780f2d252f9815b7bf
Theo Ajuyah <44715145+Theo-0@users.noreply.github.com>
2018-11-08 13:39:02 +0000

    Update rover_move.ino
    Took note of absolute value of functions accepting -ve values.
    Realised different locomotion distances require different scaling factors

--- Pied Piper (Genuino Uno)/rover_move/rover_move.ino
+++ Pied Piper (Genuino Uno)/rover_move/rover_move.ino
@@ -1,9 +1,10 @@
 /* Declare pins connected to respective s̶e̶r̶v̶o̶s̶ DC gear motors */
 // Rear Motors
-const int vel_r = 5; //M2 Speed Control (E2)
-const int vel_l = 6; //M1 Speed Control (E1)
-const int dir_r = 7; //M2 Direction Control (M2)
-const int dir_l = 8; //M1 Direction Control (M1)
+const int VEL_R = 5; //M2 Speed Control (E2)
+const int VEL_L = 6; //M1 Speed Control (E1)
+const int DIR_R = 7; //M2 Direction Control (M2)
+const int DIR_L = 8; //M1 Direction Control (M1)
+// May replace const int's w/ #define to save on storage space
 
 // ____________________________________________________________________________________________________ //
 /******************************************************************************/
@@ -37,9 +38,9 @@ void loop() {
     delay(5000);
 
     // Test moving forward and back
-    //move_forward_cm(30);
+    //move_forward_mm(30);
     //delay(5000);
-    //move_forward_cm(-30);
+    //move_forward_mm(-30);
     //delay(5000);
     
     // if executed 4 times, stop:
@@ -58,38 +59,55 @@ void loop() {
 
 // ---------------------------------------- ~ Helper functions ~ ----------------------------------------
 
-
+// Halt
 void stop() { //Stop subroutine (Halt movement)
   
-  digitalWrite(vel_l,LOW); // Used digital to ensure it's fully off
-  digitalWrite(vel_r,LOW);
+  digitalWrite(VEL_L,LOW); // Used digital to ensure it's fully off
+  digitalWrite(VEL_R,LOW);
   Serial.println("stop");
 }
 
+// Rotate - Turn_Right subroutine will work w/ -ve to turn left
 void turn_right_degrees (int angle) {  //Turn-Right subroutine (Half speed)
-  int duration = (int)(angle * 21); // delay time in ms
   
-  analogWrite (vel_l,127);   //Left  - half speed (value of 127 = half speed)
-  digitalWrite(dir_l,LOW);   //Left  - direction control
-  analogWrite (vel_r,127);   //Right - half speed (value of 127 = half speed)
-  digitalWrite(dir_r,HIGH);  //Right - direction control
+  int duration = (int)(abs(angle) * 21); // delay time in ms
+  
+  if (angle > 0) { // Right
+    analogWrite (VEL_L,127);   //Left  - half speed (value of 127 = half speed)
+    digitalWrite(DIR_L,LOW);   //Left  - direction control
+    analogWrite (VEL_R,127);   //Right - half speed (value of 127 = half speed)
+    digitalWrite(DIR_R,HIGH);  //Right - direction control
+  }
+  else { // Left
+    analogWrite (VEL_L,127);   //Left  - half speed (value of 127 = half speed)
+    digitalWrite(DIR_L,HIGH);  //Left  - direction control
+    analogWrite (VEL_R,127);   //Right - half speed (value of 127 = half speed)
+    digitalWrite(DIR_R,LOW);   //Right - direction control
+  }
+
   //String message = "right by " + (String)angle + " degrees";
   //Serial.println(message);
   delay(duration);
-  stop();
+  stop();  
 }
 
-// ***** Test Forward subroutine (currently about a cm @ distance = 1)
-//Needs argument to specify distance(maybe later in mm), will work w/ -ve to reverse
-void move_forward_cm(int distance) {
+// Locomotion(Translocate) - Forward subroutine will work w/ -ve to reverse
+void move_forward_mm(int distance) {
   int dir = (distance > 0) ? LOW : HIGH; // (LOW is forwards FOR OUR CONFIGURATION)
   
-  analogWrite (vel_l,255);  //Left  - full speed (value of 255 is max speed)
-  digitalWrite(dir_l,dir);  //Left  - direction control (LOW is forwards FOR OUR CONFIGURATION)
-  analogWrite (vel_r,255);  //Right - full speed (value of 255 = full speed)
-  digitalWrite(dir_r,dir);  //Right - direction control
-  String msg = "forward by " + (String)distance + " cm";
-  Serial.println(msg);
-  delay(distance * 100);
+  analogWrite (VEL_L,255);  //Left  - full speed (value of 255 is max speed)
+  digitalWrite(DIR_L,dir);  //Left  - direction control (LOW is forwards FOR OUR CONFIGURATION)
+  analogWrite (VEL_R,255);  //Right - full speed (value of 255 = full speed)
+  digitalWrite(DIR_R,dir);  //Right - direction control
+  //String msg = "forward by " + (String)distance + " mm";
+  //Serial.println(msg);
+
+  // Scale delay time depending on distance due to driving force
+  // required to overcome static friction from rest
+  if(abs(distance) > 50)
+    delay( int(abs(distance) * 8.2) );
+  else
+    delay( int(abs(distance) * 12) );
+  
   stop();
 }
