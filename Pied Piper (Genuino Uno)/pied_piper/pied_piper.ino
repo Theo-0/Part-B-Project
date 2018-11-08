e1a012dd2d0a8e621cc09c757438a64dc494ed6f
Theo Ajuyah <44715145+Theo-0@users.noreply.github.com>
2018-11-08 13:58:47 +0000

    Update pied_piper.ino
    Updates:
    - motion controls
    - replaced Phase 2 plans w/ Phase 1
    - tried to make it look like the flowchart

--- Pied Piper (Genuino Uno)/pied_piper/pied_piper.ino
+++ Pied Piper (Genuino Uno)/pied_piper/pied_piper.ino
@@ -1,18 +1,17 @@
 /****************************************/
-/***      BEWARE LOCAL MAXIMA!!!      ***/
+/***      BEWARE LOCAL MA✘IMA!!!      ***/
 /****************************************/
 
 /* ***** Red Light Detection(May just use gel filter paper)
 /* ***** Getting music frequencies(perhaps analyze piece in MATLAB and import array of freqs)
 /* ***** Music Detection(?)
-
+ */
 /* Declare pins connected to respective s̶e̶r̶v̶o̶s̶ DC gear motors */
 // Rear Motors
-const int VEL_R = 5; //M2 Speed Control (E2)
-const int VEL_L = 6; //M1 Speed Control (E1)
-const int DIR_R = 7; //M2 Direction Control (M2)
-const int DIR_L = 8; //M1 Direction Control (M1)
-// May replace const int's w/ #define to save on storage spaces
+#define VEL_R 5 //M2 Speed Control (E2)
+#define VEL_L 6 //M1 Speed Control (E1)
+#define DIR_R 7 //M2 Direction Control (M2)
+#define DIR_L 8 //M1 Direction Control (M1)
 
 #define LIGHT_MAX 240 // light max threshold to stop
 // ____________________________________________________________________________________________________ //
@@ -38,15 +37,21 @@ void loop() {
     static int dist = 10; // 10 cm starting loco dist
     //static const int LIGHT_MAX = 240; // light max threshold to stop
     
-    if(!face_light()) face_sound();
-    move_forward_cm(dist);
-    dist = ((getValue() > LIGHT_MAX-40) && (get_ultra_d_cm() < 5)) ? dist : dist / 2;
+    //if(!face_light()) face_sound(); // Uncomment in Phase 2
+    face_light();
+    adjust(dist);
+    move_forward_mm(dist);
 
-    if((getValue() > LIGHT_MAX) || (get_ultra_d_cm() < 1)) {
+    //if((getValue() > LIGHT_MAX) || (get_ultra_d_cm() < 1)) { // Uncomment in Phase 2
+    if((getValue() > LIGHT_MAX)) {
       // Celebrate w/ speaker, led or lcd display!
       //fun();
       done();
     }
+//    move_forward_mm(10); // ✓
+//    move_forward_mm(50); // ✗?
+//    move_forward_mm(100);// ✓
+    done();
     
 }
 
@@ -65,67 +70,52 @@ void stop() { //Stop subroutine (Halt movement)
   
   digitalWrite(VEL_L,LOW); // Used digital to ensure it's fully off
   digitalWrite(VEL_R,LOW);
-  Serial.println("stop");
+  //Serial.println("stop");
 }
 
-// Rotate
+// Rotate - Turn_Right subroutine will work w/ -ve to turn left
 void turn_right_degrees (int angle) {  //Turn-Right subroutine (Half speed)
-  int duration = (int)(angle * 21); // delay time in ms
-  
-  analogWrite (VEL_L,127);   //Left  - half speed (value of 127 = half speed)
-  digitalWrite(DIR_L,LOW);   //Left  - direction control
-  analogWrite (VEL_R,127);   //Right - half speed (value of 127 = half speed)
-  digitalWrite(DIR_R,HIGH);  //Right - direction control
-  //String message = "right by " + (String)angle + " degrees";
-  //Serial.println(message);
-  delay(duration);
-  stop();
-}
-// Rotate
-void turn_degrees (int angle) {  //Turn-Right subroutine (Half speed)
-  
   
   int duration = (int)(abs(angle) * 21); // delay time in ms
   
-  if (angle>=0)
-  {
-	analogWrite (VEL_L,127);   //Left  - half speed (value of 127 = half speed)
-	digitalWrite(DIR_L,LOW);   //Left  - direction control
-	analogWrite (VEL_R,127);   //Right - half speed (value of 127 = half speed)
-	digitalWrite(DIR_R,HIGH);  //Right - direction control
-	
-	delay(duration);
-	
-	stop();
+  if (angle > 0) { // Right
+    analogWrite (VEL_L,127);   //Left  - half speed (value of 127 = half speed)
+    digitalWrite(DIR_L,LOW);   //Left  - direction control
+    analogWrite (VEL_R,127);   //Right - half speed (value of 127 = half speed)
+    digitalWrite(DIR_R,HIGH);  //Right - direction control
   }
-  else
-  {
-	analogWrite (VEL_L,127);   //Left  - half speed (value of 127 = half speed)
-	digitalWrite(DIR_L,HIGH);   //Left  - direction control
-	analogWrite (VEL_R,127);   //Right - half speed (value of 127 = half speed)
-	digitalWrite(DIR_R,LOW);  //Right - direction control
-	
-	delay(duration);
-	
-	stop();
+  else { // Left
+    analogWrite (VEL_L,127);   //Left  - half speed (value of 127 = half speed)
+    digitalWrite(DIR_L,HIGH);  //Left  - direction control
+    analogWrite (VEL_R,127);   //Right - half speed (value of 127 = half speed)
+    digitalWrite(DIR_R,LOW);   //Right - direction control
   }
-  
+
+  //String message = "right by " + (String)angle + " degrees";
+  //Serial.println(message);
+  delay(duration);
+  stop();  
 }
 
 
-// Locomotion(Translocate)
-// ***** Test Forward subroutine (currently about a cm @ distance = 1)
-//Needs argument to specify distance(maybe later in mm), will work w/ -ve to reverse
-void move_forward_cm(int distance) {
+// Locomotion(Translocate) - Forward subroutine will work w/ -ve to reverse
+void move_forward_mm(int distance) {
   int dir = (distance > 0) ? LOW : HIGH; // (LOW is forwards FOR OUR CONFIGURATION)
   
   analogWrite (VEL_L,255);  //Left  - full speed (value of 255 is max speed)
   digitalWrite(DIR_L,dir);  //Left  - direction control (LOW is forwards FOR OUR CONFIGURATION)
   analogWrite (VEL_R,255);  //Right - full speed (value of 255 = full speed)
   digitalWrite(DIR_R,dir);  //Right - direction control
-  //String msg = "forward by " + (String)distance + " cm";
+  //String msg = "forward by " + (String)distance + " mm";
   //Serial.println(msg);
-  delay(distance * 100);
+
+  // Scale delay time depending on distance due to driving force
+  // required to overcome static friction from rest
+  if(abs(distance) > 50)
+    delay( int(abs(distance) * 8.2) );
+  else
+    delay( int(abs(distance) * 12) );
+  
   stop();
 }
 
@@ -165,6 +155,10 @@ bool face_light() {
 int get_ultra_d_cm() {
   // s = v * t (v = speed of sound in dry air at 20 °C. Adjust if needed)
   return 343 * getValue() * 100;
+}
+
+void adjust(int& distance) {
+  distance = ((getValue() > LIGHT_MAX-40) || (get_ultra_d_cm() < 5)) ? (int)(distance / 2) : distance;
 }
 
 // Might take 3-5 times quickly then avg. Depends on quality & speed performance
