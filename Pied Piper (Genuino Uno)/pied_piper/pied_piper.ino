1b9c001b039321899543b199c14d8ad23879e3d2
Theo Ajuyah <44715145+Theo-0@users.noreply.github.com>
2018-11-25 17:42:29 +0000

    Update pied_piper.ino
    Integrated light sensing code

--- Pied Piper (Genuino Uno)/pied_piper/pied_piper.ino
+++ Pied Piper (Genuino Uno)/pied_piper/pied_piper.ino
@@ -16,6 +16,9 @@
 // SparkFun Sound Detector (with Headers) [SEN-14262, $11.95]
 #define PIN_ANALOG_IN A0 // (analog input 0)
 
+// RS TEPT4400 Phototransistor
+#define PIN_PHOTO_IN  A3 // (analog input 3)
+
 // HC-SR04 Ultrasonic sensor
 #define TRIG_PIN 9
 #define ECHO_PIN 10
@@ -40,6 +43,7 @@ void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   //pinMode(ECHO_PIN, INPUT);
   //pinMode(PIN_ANALOG_IN, INPUT);
+  //pinMode(PIN_PHOTO_IN, INPUT);
   
   pinMode(PIN_LED_OUT, OUTPUT); //  Configure LED pin as output
 
@@ -76,15 +80,24 @@ void loop() {
     //face_sound();
 
     ////Face sound, and wait
-    //face_sound_720();
-    //digitalWrite(PIN_LED_OUT,HIGH);
-    //delay(5000);
-    //digitalWrite(PIN_LED_OUT,LOW);
-    //delay(5000);
+    face_ultra_480();
+    float s = get_ultra_d_cm();
+    while(s > 11) {
+      move_forward_mm(10);
+      delay(100);
+      s = get_ultra_d_cm();
+    }
     
-    float distance_cm = get_ultra_d_cm();
-    Serial.println("Distance: " + (String)distance_cm);
-    delay(100); // every 1/10sec.
+//    face_light_480();
+    
+    digitalWrite(PIN_LED_OUT,HIGH);
+    delay(5000);
+    digitalWrite(PIN_LED_OUT,LOW);
+    delay(5000);
+    
+//    float distance_cm = get_ultra_d_cm();
+//    Serial.println("Distance: " + (String)distance_cm);
+//    delay(100); // every 1/10sec.
     
 }
 
@@ -176,6 +189,70 @@ void adjust(int& distance) {
   distance = ((getValue() > LIGHT_MAX-40) || (get_ultra_d_cm() < 5)) ? (int)(distance / 2) : distance;
 }
 
+// Face_Ultrasound_Test_Simulation
+void face_ultra_480_sim() {
+  int dirs[8] = {115,75,65,13,124,60,130,55};
+  //              0  1  2  |3| 4  5   6   7
+  int theta = 60;
+  int total_turns = 480/theta;
+  int minIndex;
+  int minValue = dirs[0];
+  
+  // 480 check
+  for(int i=0; i<total_turns; i++) {
+    Serial.println(dirs[i]);
+    if(dirs[i] < minValue) {
+      minValue = dirs[i];
+      minIndex = i;
+    }
+  }
+
+  Serial.println("Now going back. MinVal was " + (String)minValue + " at index " + (String)minIndex);
+  for(int i=total_turns-1; i>=minIndex; i--)
+    Serial.println(dirs[i]);
+  
+}
+// Face closest thing using ultrasound
+void face_ultra_480() {
+  int theta = 15;
+  int total_turns = 480/theta;
+  float directions[total_turns] = {0};
+  int minIndex=0;
+  float minValue = get_ultra_d_cm();
+
+  digitalWrite(PIN_LED_OUT,HIGH);
+  
+  // 480 check
+  for(int i=0; i<total_turns; i++) { // **** Keep checking. Forever if 480° surround-sound
+    directions[i] = get_ultra_d_cm();
+    if(directions[i] < minValue) {
+      minValue = directions[i];
+      minIndex = i;
+    }
+    turn_right_degrees(theta);
+    Serial.println(directions[i]);
+    delay(10);
+  }
+
+  digitalWrite(PIN_LED_OUT,LOW);
+  delay(5000);
+  
+  Serial.println("Now going back. MinVal was " + (String)minValue + " at index " + (String)minIndex);
+  //turn_right_degrees((total_turns-minIndex-1) * -theta); // Doesn't match original motion
+  // e.g. if 10 turns(0-9) and u want 3. Turn back 6 (i.e. 9-3)
+
+  digitalWrite(PIN_LED_OUT,HIGH);
+  
+  // Go back in similar short bursts to mimic/match original motion
+  for(int i=total_turns-1; i>=minIndex; i--) {
+    turn_right_degrees(-theta);
+    Serial.println(directions[i]);
+    delay(10);
+  }
+  digitalWrite(PIN_LED_OUT,LOW);
+  delay(5000);
+}
+
 void face_sound_720() {
   int theta = 60;
   int total_turns = 720/theta;
@@ -225,10 +302,34 @@ void face_sound() {
   
 }
 
-bool face_light() {
-  int cur;// ditto w/ sound. Can change ϑ
-  int light_min = 40; // light min threshold to trust
-  return (cur > light_min);
+bool face_light_480() {
+  int light_min = 205; // approx a fifth of supply
+  int theta = 15;
+  int total_turns = 480/theta;
+  float directions[total_turns] = {0};
+  int maxIndex=0;
+  float maxValue=0;
+
+  // 480 check
+  for(int i=0; i<total_turns; i++) { // **** Keep checking. Forever if 480° surround-sound
+    directions[i] = getLight();
+    if(directions[i] > maxValue) {
+      maxValue = directions[i];
+      maxIndex = i;
+    }
+    turn_right_degrees(theta);
+    Serial.println(directions[i]);
+  }
+
+  Serial.println("Now going back. MaxVal was " + (String)maxValue + " at index " + (String)maxIndex);
+  
+  // Go back in similar short bursts to mimic/match original motion
+  for(int i=total_turns-1; i>=maxIndex; i--) {
+    turn_right_degrees(-theta);
+    Serial.println(directions[i]);
+  }
+  
+  return (maxValue > light_min);
 }
 
 // --------------------------------------------- ~ Sensory Systems ~ ---------------------------------------------
@@ -242,17 +343,34 @@ int getSound() {
   return sval / samples; // avg
 }
 
+int getLight() {
+  int sval = 0;
+  int samples = 3;
+  for(int i=0; i<samples; i++)
+    sval += analogRead(PIN_PHOTO_IN);
+  return sval / samples;
+}
+
 // Turns ultrasound echo time to distance
 float get_ultra_d_cm() {
-  // Generate ultrasonic wave
-  digitalWrite(TRIG_PIN, LOW); 
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-
-  // Read the echoPin, returns the sound wave travel time in microseconds
-  unsigned long duration = pulseIn(ECHO_PIN, HIGH);
+  unsigned long duration;
+  unsigned long sval = 0; // sum value
+  int samples = 1;
+  for(int i=0; i<samples; i++) {
+    // Generate ultrasonic wave
+    digitalWrite(TRIG_PIN, LOW); 
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+  
+    // Read the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(ECHO_PIN, HIGH);
+    sval += duration;
+    
+    //delay(1);
+  }
+  duration = sval / samples;
   
   // Calculate & return distance: s = t/2 * c (t = time taken to obj & back!)
   return (duration/2) * .0343; // (c = speed of sound in dry air at 20 °C in cm/μs)
