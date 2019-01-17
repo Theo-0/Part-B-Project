19f5c36718abe5bf5ba928b8e8939d906f612152
Theo Ajuyah <44715145+Theo-0@users.noreply.github.com>
2019-01-17 21:57:08 +0000

    Update light_test.ino
    - added optional ground and power pins (the same ones used in the Pied Piper program when the light and ultrasound system where connected directly to the Arduino)
    - changed the analogue input pin from A3 to A5(as in the Pied Piper program)
    - lowered the LED indicator threshold value from 4V to 2.5V to make checks easier

--- Pied Piper (Genuino Uno)/light_test/light_test.ino
+++ Pied Piper (Genuino Uno)/light_test/light_test.ino
@@ -5,17 +5,28 @@
  * // https://learn.parallax.com/tutorials/robot/shield-bot/robotics-board-education-shield-arduino/chapter-6-light-sensitive-14
  */
 #define led 13 // Test LED
+// virtual earth & power for 2nd phototransistor
+#define GND2 2
+#define VCC2 3
+
 void setup() {                               // Built-in initialization block
   pinMode(led, OUTPUT);                      // Configure LED pin as output
   Serial.begin(9600);                        // Set data rate to 9600 bps
+  // Pull up to operating voltage(5V)
+
+//Uncomment these lines if you wish to make them virtual power & earth
+  //pinMode(VCC2, OUTPUT);
+  //digitalWrite(VCC2, HIGH);
+  //pinMode(GND2, OUTPUT);
+  //digitalWrite(GND2, LOW);
 }
 
 void loop() {                                // Main loop auto-repeats
-  Serial.print("A3 = ");                     // Display "A3 = "
-  Serial.print(volts(A3));                   // Display measured A3 volts
+  Serial.print("A5 = ");                     // Display "A5 = ", used to be A3
+  Serial.print(volts(A5));                   // Display measured A5 volts********************
   Serial.println(" volts");                  // Display " volts" & newline
 
-  if(volts(A3) > 4)
+  if(volts(A5) > 2.5)
     digitalWrite(led,HIGH);
   else
     digitalWrite(led,LOW);
